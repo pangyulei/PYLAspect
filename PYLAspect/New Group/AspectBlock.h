2dc11cfcf19ccb31a3c249c505017db816da9574
yulei pang <yuleipang@yuleideMacBook-Pro.local>
2019-02-16 05:32:35 +0800

    实现类方法的消息转发

--- PYLAspect/New Group/AspectBlock.h
+++ PYLAspect/New Group/AspectBlock.h
@@ -1,12 +1,13 @@
 //
-//  NSObject+Aspect.m
+//  AspectBlock.h
 //  PYLAspect
 //
 //  Created by yulei pang on 2019/2/16.
 //  Copyright © 2019 pangyulei. All rights reserved.
 //
 
-#import "NSObject+Aspect.h"
+#ifndef AspectBlock_h
+#define AspectBlock_h
 
 //此处 AspectBlock 的定义拷贝自 Clang 文档 http://clang.llvm.org/docs/Block-ABI-Apple.html
 enum {
@@ -25,36 +26,30 @@ enum {
     BLOCK_HAS_SIGNATURE =     (1 << 30),
 };
 
-typedef struct {
+struct AspectBlockDesc {
+    unsigned long int reserved;         // NULL
+    unsigned long int size;         // sizeof(struct Block_literal_1)
+    //        // optional helper functions
+    //        void (*copy_helper)(void *dst, void *src);     // IFF (1<<25)
+    //        void (*dispose_helper)(void *src);             // IFF (1<<25)
+    //        // required ABI.2010.3.16
+    //        const char *signature;                         // IFF (1<<30)
+    //因为 copy_helper 和 dispose_helper 是不一定存在的，所以这里用 extra[1] 去接，c 语言中 int a[1] = {1,2,3} 是不会出错的
+    void *extras[1];
+};
+typedef struct AspectBlockDesc AspectBlockDesc;
+
+struct AspectBlock {
     void *isa; // initialized to &_NSConcreteStackBlock or &_NSConcreteGlobalBlock
     int flags;
     int reserved;
     void (*invoke)(void *, ...);
-    struct AspectBlockDesc {
-        unsigned long int reserved;         // NULL
-        unsigned long int size;         // sizeof(struct Block_literal_1)
-//        // optional helper functions
-//        void (*copy_helper)(void *dst, void *src);     // IFF (1<<25)
-//        void (*dispose_helper)(void *src);             // IFF (1<<25)
-//        // required ABI.2010.3.16
-//        const char *signature;                         // IFF (1<<30)
-        //因为 copy_helper 和 dispose_helper 是不一定存在的，所以这里用 extra[1] 去接，c 语言中 int a[1] = {1,2,3} 是不会出错的
-        void *extras[1];
-    } *descriptor;
+    AspectBlockDesc *descriptor;
     // imported variables
-} AspectBlock;
+};
+typedef struct AspectBlock AspectBlock;
 
-@implementation NSObject (Aspect)
-
-+ (void)aspect_selector:(SEL)sel options:(AspectOptions)opt block:(id)blk {
-    
-}
-
-- (void)aspect_selector:(SEL)sel options:(AspectOptions)opt block:(id)blk {
-    
-}
-
-NSMethodSignature * __block_methodSignature(id blk) {
+NSMethodSignature * blockMethodSignature(id blk) {
     AspectBlock *aspectBlk = (__bridge void *)blk;
     //判断有没有 signature
     if (!(aspectBlk->flags & BLOCK_HAS_SIGNATURE)) {
@@ -70,4 +65,4 @@ NSMethodSignature * __block_methodSignature(id blk) {
     }
 }
 
-@end
+#endif /* AspectBlock_h */
